2a17242d4f7e22588f43c5021311e95bac16b9ac
Prince Sanguan <princesanguan44@gmail.com>
2024-01-21 18:19:20 +0800

    change - header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Work From Home</title>
+    <title>Apply Easy Job!</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
